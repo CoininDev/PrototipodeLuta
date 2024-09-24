5e6520f0042c183aee9499779b1491fc07a02ab4
CoininDev <gabr.batz@gmail.com>
2024-09-24 16:52:50 -0300

    kuririn

--- scripts/States/AtaqueAereo.gd
+++ scripts/States/AtaqueAereo.gd
@@ -4,6 +4,7 @@ class_name AtaqueAereoState
 func enter():
 	anim.play(player.current_attack.animation)
 	anim.animation_finished.connect(_on_anim_end)
+	player.animspr.play(player.current_attack.sprites_name)
 
 func _on_anim_end(_anim_name: StringName):
 	emit_signal("Transitioned", self, "fall")
--- scripts/state.gd
+++ scripts/state.gd
@@ -10,11 +10,19 @@ func exit() -> void: pass
 func update(_delta:float) -> void: pass
 func physics_update(_delta:float) -> void: pass
 func attack(aereo:bool) -> void:
+	var attacked: bool
 	##Ataques
 	if Input.is_action_just_pressed("atk1") and !player.dummy:
 		player.current_attack = player.resource.attacks[0]
+		attacked = true
+	if Input.is_action_just_pressed("atk2") and !player.dummy:
+		player.current_attack = player.resource.attacks[1]
+		attacked = true
+	
+	if attacked:
 		if aereo:
 			emit_signal("Transitioned", self, "ataqueAereo")
 		else:
 			emit_signal("Transitioned", self, "ataque")
+	
 	##...
